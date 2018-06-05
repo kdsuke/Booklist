b05a5a8f599deee68f2af379847deb7f251ac815
kdsuke <reidness1910@gmail.com>
2018-06-05 02:39:03 +0000

    modify

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -3,22 +3,25 @@
 @section('content')
 
 
-<h1>読み終わった本の追加ページ</h1>
+
 
 <div class="row">
-        <div class="col-xs-12">
-
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+<h1>読み終わった本の追加ページ</h1>
     {!! Form::model($book, ['route' => 'books.store']) !!}
 
-　　　　{!! Form::label('title', 'タイトル:') !!}
-        {!! Form::text('title') !!}
-        
-        {!! Form::label('content', 'ジャンル:') !!}
-        {!! Form::text('content') !!}
-        
-        {!! Form::label('author', '著者:') !!}
-        {!! Form::text('author') !!}
-        
+        <div class="form-group">
+{!! Form::label('title', 'タイトル:') !!}
+           {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+           {!! Form::label('content', 'ジャンル:') !!}
+           {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+           {!! Form::label('author', '著者:') !!}
+           {!! Form::text('author', null, ['class' => 'form-control']) !!}
+        </div>
 
         {!! Form::submit('投稿', ['class' => 'btn btn-primary'])!!}
 
--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -4,22 +4,27 @@
 
 
 
-<h1>id: {{ $book->id }} の編集ページ</h1>
+
 
 
 <div class="row">
-        <div class="col-xs-12">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+                <h1>id: {{ $book->id }} の編集ページ</h1>
+                
     {!! Form::model($book, ['route' => ['books.update', $book->id], 'method' => 'put']) !!}
 
+         <div class="form-group">
          {!! Form::label('title', 'タイトル:') !!}
-        {!! Form::text('title') !!}
-        
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
         {!! Form::label('content', 'ジャンル:') !!}
-        {!! Form::text('content') !!}
-        
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
         {!! Form::label('author', '著者:') !!}
-        {!! Form::text('author') !!}
-
+        {!! Form::text('author', null, ['class' => 'form-control']) !!}
+        </div>
         {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
